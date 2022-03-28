e3b81bfeebb05e853536e626a9d639f9c6d48456
qq1041663097 <1041663097@qq.com>
2022-03-28 23:29:02 +0800

    :package:Sakura主题文件

---
+++ footer.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Sakura
+ */
+
+?>
+	</div><!-- #content -->
+	<?php 
+		if(akina_option('general_disqus_plugin_support')){
+			get_template_part('layouts/duoshuo');
+		}else{
+			comments_template('', true); 
+		}
+	?>
+	</div><!-- #page Pjax container-->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info" theme-info="Sakura v<?php echo SAKURA_VERSION; ?>">
+			<div class="footertext">
+				<div class="img-preload">
+					<img src="https://upyun.rm-rf.fun/sakura/icon/wordpress-rotating-ball-o.svg">
+					<img src="https://upyun.rm-rf.fun/sakura/icon/disqus-preloader.svg">
+				</div>
+				<p style="color: #666666;"><?php echo akina_option('footer_info', ''); ?></p>
+			</div>
+			<div class="footer-device">
+				<div class="footer-column">
+				    <div class="footer-column-item">
+				        <img class="pong-pong" style="width:16px;height:16px;margin-right:3px;" src="https://upyun.rm-rf.fun/static/baozi.png">
+                        <a href="https://rm-rf.fun/" target="_blank">竟然有人抢包</a>
+				    </div>
+				    <div class="footer-column-item">
+                    	<img style="width:16px;height:16px;" src="https://upyun.rm-rf.fun/static/icp.png">
+                        <a href="http://beian.miit.gov.cn/" target="_blank">桂ICP备19009695号-1</a>
+                    </div>
+			    </div>
+                <div class="footer-column">
+                    <div class="footer-column-item">
+                        <img style="width:16px;height:16px;" src="https://upyun.rm-rf.fun/static/icomoon.png">
+                        <a href="https://icp.gov.moe/?keyword=20229695" target="_blank">萌ICP备20229695号</a>
+                    </div>
+                    <div class="footer-column-item">
+                        <i class="iconfont icon-sakura rotating" style="color: #ffc0cb;display:inline-block"></i>
+                        <a href="https://2heng.xin/theme-sakura/" target="_blank">Sakura</a>主题修改
+                    </div>
+                    <div class="footer-column-item">
+                    	<a href="https://www.upyun.com/?utm_source=lianmeng&amp;utm_medium=referral" target="_blank" style="margin-right: 0;">
+                        <img src="https://upyun.rm-rf.fun/static/upyun_logo5.png" style="height: 25px;margin-bottom: -7px;">
+                        </a>提供云存储
+                    </div>
+                </div>
+			</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+	</section><!-- #section -->
+	<!-- m-nav-center -->
+	<div id="mo-nav">
+		<div class="m-avatar">
+			<?php $ava = akina_option('focus_logo') ? akina_option('focus_logo') : get_template_directory_uri().'/images/avatar.jpg'; ?>
+			<img src="<?php echo $ava ?>">
+		</div>
+		<div class="m-search">
+			<form class="m-search-form" method="get" action="<?php echo home_url(); ?>" role="search">
+				<input class="m-search-input" type="search" name="s" placeholder="<?php _e('Search...', 'sakura') /*搜索...*/?>" required>
+			</form>
+		</div>
+		<?php wp_nav_menu( array( 'depth' => 2, 'theme_location' => 'primary', 'container' => false ) ); ?>
+	</div><!-- m-nav-center end -->
+	<a class="cd-top faa-float animated "></a>
+	<button id="moblieGoTop" title="Go to top"><i class="fa fa-chevron-up" aria-hidden="true"></i></button>
+	<button id="moblieDarkLight"><i class="fa fa-moon-o" aria-hidden="true"></i></button>
+	<!-- search start -->
+	<form class="js-search search-form search-form--modal" method="get" action="<?php echo home_url(); ?>" role="search">
+		<div class="search-form__inner">
+		<?php if(akina_option('live_search')){ ?>
+			<div class="micro">
+				<i class="iconfont icon-search"></i>
+				<input id="search-input" class="text-input" type="search" name="s" placeholder="<?php _e('Want to find something?', 'sakura') /*想要找点什么呢*/?>" required>
+			</div>
+			<div class="ins-section-wrapper">
+                <a id="Ty" href="#"></a>
+                <div class="ins-section-container" id="PostlistBox"></div>
+            </div>
+		<?php }else{ ?>
+			<div class="micro">
+				<p class="micro mb-"><?php _e('Want to find something?', 'sakura') /*想要找点什么呢*/?></p>
+				<i class="iconfont icon-search"></i>
+				<input class="text-input" type="search" name="s" placeholder="<?php _e('Search', 'sakura') ?>" required>
+			</div>
+		<?php } ?>
+		</div>
+		<div class="search_close"></div>
+	</form>
+	<!-- search end -->
+<?php wp_footer(); ?>
+<?php if(akina_option('site_statistics')){ ?>
+<div class="site-statistics">
+<script type="text/javascript"><?php echo akina_option('site_statistics'); ?></script>
+</div>
+<?php } ?>
+<div class="changeSkin-gear no-select" style="bottom: -999px;">
+    <div class="keys">
+        <span id="open-skinMenu">
+		<i class="iconfont icon-gear inline-block rotating"></i>&nbsp; 切换主题
+        </span>
+    </div>
+</div>
+<div class="skin-menu no-select">
+    <div class="theme-controls row-container">
+        <ul class="menu-list">
+            <li id="white-bg">
+                <i class="fa fa-television" aria-hidden="true"></i>
+            </li><!--Default-->
+            <li id="sakura-bg">
+                <i class="iconfont icon-sakura"></i>
+            </li><!--Sakura-->
+            <li id="gribs-bg">
+                <i class="fa fa-slack" aria-hidden="true"></i>
+            </li><!--Grids-->
+            <li id="KAdots-bg">
+                <i class="iconfont icon-dots"></i>
+            </li><!--Dots-->
+            <li id="totem-bg">
+                <i class="fa fa-superpowers" aria-hidden="true"></i>
+            </li><!--Orange-->
+            <li id="pixiv-bg">
+                <i class="iconfont icon-pixiv"></i>
+            </li><!--Start-->
+            <li id="bing-bg">
+                <i class="iconfont icon-bing"></i>
+            </li><!--Bing-->
+            <li id="dark-bg">
+                <i class="fa fa-moon-o" aria-hidden="true"></i>
+            </li><!--Night-->
+        </ul>
+    </div>
+    <div class="font-family-controls row-container">
+        <button type="button" class="control-btn-serif selected" data-mode="serif" 
+                onclick="mashiro_global.font_control.change_font()">这个字体</button>
+        <button type="button" class="control-btn-sans-serif" data-mode="sans-serif" 
+                onclick="mashiro_global.font_control.change_font()">那个字体</button>
+    </div>
+</div>
+<canvas id="night-mode-cover"></canvas>
+<?php if (akina_option('sakura_widget')) : ?>
+	<aside id="secondary" class="widget-area" role="complementary" style="left: -400px;">
+    <div class="heading"><?php _e('Widgets') /*小工具*/ ?></div>
+    <div class="sakura_widget">
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sakura_widget')) : endif; ?>
+	</div>
+	<div class="show-hide-wrap"><button class="show-hide"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 32 32"><path d="M22 16l-10.105-10.6-1.895 1.987 8.211 8.613-8.211 8.612 1.895 1.988 8.211-8.613z"></path></svg></button></div>
+    </aside>
+<?php endif; ?>
+</body>
+</html>
